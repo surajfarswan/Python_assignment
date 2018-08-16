213cd7581503bededc915da4ca0e926bd9962937
trishali <trishali008@gmail.com>
2018-08-17 00:42:27 +0530

    This is second assignment.

---
+++ Assignment2.py
@@ -0,0 +1,33 @@
+# Print anything on screen
+
+print("Hey there!It's Thursday Today.")
+
+# join 2 strings
+
+print('Valar' + ' Morghulis')
+
+# Take input and print their values
+
+a = int(input('Enter value of a: '))
+b = int(input('Enter value of b: '))
+c = int(input('Enter value of c: '))
+print('Value of a is: ',a)
+print('Value of b is: ',b)
+print('Value of c is: ',c)
+
+# print "Let's get started"
+
+print("Let's get started.")
+
+# print using placeholders
+
+s = 'Acadview'
+course = 'Python'
+fees = 5000
+print("{} company offers a course of {} only for {} Rs".format(s,course,fees))
+
+# Interesting exercise
+
+name = 'Tony Stark'
+sal = 1000000
+print(('%s " %d')%(name,sal))

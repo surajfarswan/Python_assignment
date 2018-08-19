db2a2b23139d41d6dd06f4d5c0bc9f6c481cc208
trishali <trishali008@gmail.com>
2018-08-19 10:53:14 +0530

    problem1

--- problem1.py
+++ problem1.py
@@ -1,3 +1,3 @@
-name,age = input('Enter name: '),int(input('Enter age: '))
-year = 1999 + 100 - age
+name,age,birth = input('Enter name: '),int(input('Enter age: ')),int(input('Enterbirth year: '))
+year = birth + 100 - age
 print("Hello {},your age is {} and the year you'll turn 100 is {}".format(name,age,year))
